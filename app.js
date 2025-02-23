2629bf55d78df7e099e681c11264979f291e93b9
Mamimi88 <majaheab@gmail.com>
2025-02-23 15:16:50 -0300

    Orden app.js

--- app.js
+++ app.js
@@ -2,32 +2,24 @@
 let amigos = []
 let listaAmigos = document.getElementById("listaAmigos"); // Se obtiene el elemento de la lista en HTML.
 
-
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value; // obtendo el valor del campo.  Ayudado por ChatGTP en corroborar.
-    console.log(agregarAmigo); // Imprime lo que el usuario haya escrito. Ayudado por ChatGTP en corroborar.
-    
-    //Se verifica si el campo está vacío
-    if (agregarAmigo === "") { 
-        alert("Por favor, inserte un nombre:");
-} else {
-        amigos.push(amigo);
-        alert('Agregado');
+        console.log(agregarAmigo); // Imprime lo que el usuario haya escrito. Ayudado por ChatGTP en corroborar.
+              if (agregarAmigo === "") {    //Se verifica si el campo está vacío
+                alert("Por favor, inserte un nombre:");
+            } else {
+                amigos.push(amigo);
+                alert('Agregado');
 
         actualizarLista();
         }
 }
-
-
-
 function actualizarLista() {
         listaAmigos.innerHTML = ""; //Limpiar la lista antes de agregar nuevos amigos para que ho haya duplicado.
-    
-        // Recorrer la lista de amigos y agregar cada uno a la lista HTML
-        for (let i = 0; i < amigos.length; i++) { // sacado topico duda while y for. 
-                let li = createElement("li");  /// repasé de nuevo el for por no entenderlo en varias paginas. 
-            li.textContent = amigos[i];
-            listaAmigos.appendChild(li);
+                    for (let i = 0; i < amigos.length; i++) { // sacado topico duda while y for.  // Recorrer la lista de amigos y agregar cada uno a la lista HTML
+                    let li = createElement("li");  /// repasé de nuevo el for por no entenderlo en varias paginas. 
+                    li.textContent = amigos[i];
+                    listaAmigos.appendChild(li);
         }
 }
 
