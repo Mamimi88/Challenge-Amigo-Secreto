7bd4d3c3f488a1e0f411fdc9ce268f77ec2465ec
Mamimi88 <majaheab@gmail.com>
2025-02-23 13:03:04 -0300

    aseets/

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-let amigos : []
+let amigos = [];
 
-document.getElementById(agregarAmigo);
-agregarAmigo.innerHTML = "Añade un amigo";
-amigos.push() /*Actualizar el array de amigos. Si el valor es válido,
-añadirlo al arreglo que almacena los nombres de amigos usando el método .push()
-*/
+document.getElementById;
+    alert("Por favor, inserte un nombre.");
 
-//Avisa al hacer clic en el botón "Añadir" un mensaje el cual es "agregado!" en una función
-function agregarAmigo() {
-  let agregarAmigo = document.getElementById('amigo').value; // Obtengo el valor del campo. 
-    console.log(agregarAmigo); //Imprime lo que el usuario haya escrito en la consola.
-
-  //Se verifica si el campo está vacio.
-  if (agregarAmigo === "") {
-    alert("Por favor, inserte un nombre:")
-  } else {
-    alert("Agregado");
-}
-
+    
